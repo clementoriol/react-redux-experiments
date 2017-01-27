22d9fd4c00ad99392a5c0305ae3725f5ff7dcefb
Clément Oriol <clement@alqemist.com>
2017-01-27 18:16:44 +0100

    Add understanding redux createStore

---
+++ 02-understanding-redux-createStore/README.md
@@ -0,0 +1,24 @@
+# 02. Understanding Redux createStore
+
+As displayed in [Dan Abramov's Egghead course on redux](https://egghead.io/lessons/javascript-redux-implementing-store-from-scratch)
+this is a basic reimplementation of Redux's createStore function.
+
+It's displayed in the same context than the previous example, [01-redux-only-counter](../01-redux-only-counter)
+
+## Usage
+
+### Install dependencies
+```
+yarn install
+```
+
+### Start a local server
+```
+yarn start
+```
+
+### Usage
+Go to [http://localhost:3000/](http://localhost:3000/) 
+
+Left click to increment counter, Right click to decrement.
+ 
---
+++ 02-understanding-redux-createStore/src/index.js
@@ -0,0 +1,56 @@
+import './index.css';
+
+const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+
+const createStore = (reducer) => {
+  let state;
+  const listeners = [];
+
+  const getState = () => {
+    return state;
+  };
+
+  const subscribe = (callback) => {
+    listeners.push(callback);
+    return () => {
+      listeners.filter(listener => listener !== callback);
+    };
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listenerCb => listenerCb());
+  };
+
+  dispatch({});
+
+  return { getState, subscribe, dispatch };
+};
+
+const store = createStore(counterReducer);
+
+const renderCounter = () => {
+  document.body.innerText = store.getState();
+};
+
+store.subscribe(renderCounter);
+renderCounter();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
+
+document.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+  store.dispatch({ type: 'DECREMENT' });
+});
